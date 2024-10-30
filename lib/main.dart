9a16dd1c93d3f24ae999cdcbf44b09ac81b4e0a4
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-30 16:39:31 -0300

    Refactor MyApp to use ValueNotifier for theme management and remove MyHomePage implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'presentation/controller/theme_controller.dart';
+import 'presentation/pages/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,70 +19,10 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp(
           title: 'Flutter Demo',
-          theme: themeController.isDarkTheme
-              ? ThemeData.dark()
-              : ThemeData.light(),
+          theme: themeController.value ? ThemeData.dark() : ThemeData.light(),
           home: const MyHomePage(),
         );
       },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    themeController.addListener(callback);
-    super.initState();
-  }
-
-  void callback() {
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ChangeNotifier'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Mudar tema do Aplicativo',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            ListenableBuilder(
-              listenable: themeController,
-              builder: (context, child) {
-                return Switch(
-                  value: themeController.isDarkTheme,
-                  onChanged: (value) {
-                    themeController.changeTheme();
-                  },
-                );
-              }
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    themeController.removeListener(callback);
-    super.dispose();
   }
 }
